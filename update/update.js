b4d29baefe2407e3c5fc7241b5ce70baf3c8236a
Sunki Baek <sunki.baek@gmail.com>
2017-12-05 12:28:26 +0900

    command에 set이 있는 경우 state를 업데이트하도록 수정

--- update/update.js
+++ update/update.js
@@ -1,5 +1,13 @@
 const update = (state, commands) => {
-  const newState = { ...state, name: "Bob" };
+  const newState = { ...state };
+
+  Object.keys(commands).forEach(key => {
+    const command = commands[key];
+
+    if (command.$set !== undefined) {
+      newState[key] = command.$set;
+    }
+  });
 
   return newState;
 };
